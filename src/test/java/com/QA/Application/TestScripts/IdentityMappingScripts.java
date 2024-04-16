72cd44be86a44ffa0f9cb00a1d5fb170a3efcd34
narendra25 <anrsmile96@gmail.com>
2024-04-16 11:10:36 +0530

    added

--- src/test/java/com/QA/Application/TestScripts/IdentityMappingScripts.java
+++ src/test/java/com/QA/Application/TestScripts/IdentityMappingScripts.java
@@ -13,53 +13,6 @@ public class IdentityMappingScripts extends TestBase {
 		//Application.IdentityMappings("Last Name");
 		//Application.IdentityMappings("Email");
 		
-<<<<<<< HEAD
-		//Launch Application
-		Launch_Application();
-		
-		//Click On setting Button icon
-		webdriver.clickOnButton(IdentityMappingPage.globalDropdown);
-		LogInFo("User click on Setting Icon");
-		TakeScreenshot("User click on Setting Icon","IdentityMappings","SettingsIcon");
-		//click on Global setting
-		webdriver.clickOnButton(IdentityMappingPage.globalSetting);
-		LogInFo("User Click On Global settings ");
-		webdriver.clickOnButton(IdentityMappingPage.identityMappings);
-		LOG.info("User Click on Identity Mapping");
-		TakeScreenshot("User Click on Identity Mapping", "IdentityMappings", "IdentityMapping");
-		
-		if(properties.getProperty("IdentityFirstName").equalsIgnoreCase("vinay")) {
-			webdriver.clickOnButton(IdentityMappingPage.firstName);
-		}
-		if(properties.getProperty("IdentityLastName").equalsIgnoreCase("LastName")) {
-			webdriver.clickOnButton(IdentityMappingPage.lastName);
-		}
-		if(properties.getProperty("IdentityEmail").equalsIgnoreCase("LastName")) {
-			webdriver.clickOnButton(IdentityMappingPage.email);
-		}	
-		else {
-			System.out.println("getting error");
-		}
-		LOG.info("User Click on Identity Attribute");
-		TakeScreenshot("User selected Identity Attribute", "IdentityMappings", "IdentityAttribute");
-		webdriver.clickOnButton(IdentityMappingPage.addSource);
-		LOG.info("User click on Add Source");
-		TakeScreenshot("User Click on Add Source", "IdentityMappings", "AddSource");
-		webdriver.enterText(IdentityMappingPage.appValue, properties.getProperty("ApplicationName"));
-		webdriver.clickOnButton(IdentityMappingPage.selectApp);
-		Thread.sleep(1000);
-		webdriver.selectTextOnDropDown(IdentityMappingPage.attributeValue, properties.getProperty("IdentityLastName").toUpperCase());
-		LOG.info("User entered Source Data");
-		TakeScreenshot("User entered Source Data", "IdentityMappings", "SourceData");
-		webdriver.clickOnButton(IdentityMappingPage.addButton);
-		webdriver.ScrollParticularElement(IdentityMappingPage.saveButton);
-		Thread.sleep(10000);
-		webdriver.clickOnButton(IdentityMappingPage.saveButton);
-		LOG.info("User Edited Identity Attribute Successfully");
-		TakeScreenshot("User Edited Identity Attribute Successfully", "IdentityMappings", "EditedAttribute");
-		
-		LogOut_Application();
-=======
->>>>>>> 755730e79cb8aab39afec81c30560b057afa6798
+
 	}
 }
